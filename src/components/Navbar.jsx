247dea2cb6ef8877f54c64975688c4aac370ca37
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-08-10 19:35:44 +0530

    navbar and slider added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Avatar from 'react-avatar'
+import { AiOutlineMenu } from "react-icons/ai";
+import { CiSearch } from "react-icons/ci";
+import logo from "../../public/logo.png";
+import { IoMdMic } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { AiOutlineBell } from "react-icons/ai";
+import profile from "../../public/profile.jpg"
+function Navbar() {
+  return (
+    <div className="flex justify-between px-6 py-2">
+      <div className="flex items-center space-x-4">
+        <AiOutlineMenu className="text-xl cursor-pointer" />
+        <img src={logo} alt="yt-logo" className="w-20 cursor-pointer" />
+      </div>
+      <div className="flex w-[35%] items-center">
+        <div className="w-[100%] px-3 py-2 border rounded-l-full">
+          <input type="text" placeholder="Search..." className="outline-none" />
+        </div>
+        <button className="px-4 py-2 border rounded-r-full bg-gray-100">
+          <CiSearch size={"24px"}/>
+        </button>
+        <IoMdMic size={"42px"} className="ml-3 border rounded-full p-2 cursor-pointer hover:bg-gray-200 duration-200"/>
+      </div>
+      <div className="flex space-x-5 items-center cursor-pointer">
+      <RiVideoAddLine className="text-2xl"/>
+      <AiOutlineBell className="text-2xl cursor-pointer"/>
+      <Avatar src={profile} size="32" round={true}/>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
