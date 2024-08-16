bb4c7adb8dfa6d2c20425b3690167ef7baab5ecb
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-08-16 18:08:00 +0530

    responsiveness added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "react-avatar";
 import { AiOutlineMenu } from "react-icons/ai";
 import { CiSearch } from "react-icons/ci";
 import logo from "/logo.png";
 import { IoMdMic } from "react-icons/io";
+import { IoSearch } from "react-icons/io5";
+import { IoArrowBack } from "react-icons/io5";
 import { RiVideoAddLine } from "react-icons/ri";
 import { AiOutlineBell } from "react-icons/ai";
 import profile from "/profile.jpg";
 import { useNavigate } from "react-router-dom";
+import { useUtils } from "../context/UtilsContext";
+
 function Navbar() {
   const [searchQuery, setSearchQuery] = useState("");
+  const { setIsSidebar, isSidebar, mobileShow, setMobileShow } = useUtils();
+  const [searchBar, setSearchBar] = useState(false);
+
+  useEffect(() => {
+    console.log({ setIsSidebar, mobileShow });
+  }, [isSidebar]);
 
   const navigate = useNavigate();
 
@@ -22,13 +32,53 @@ function Navbar() {
       setSearchQuery("");
     }
   };
+  const handleSidebar = () => {
+    if (window.innerWidth <= 1280) {
+      setIsSidebar(!isSidebar);
+      setMobileShow(!mobileShow);
+    }
+    setIsSidebar(!isSidebar);
+  };
+  if (searchBar) {
+    return (
+      <div className="flex justify-between fixed top-0 w-[100%] bg-white px-6 py-2 items-center">
+        <IoArrowBack size={20} onClick={()=>setSearchBar(!searchBar)}/>
+        <div className="flex flex-grow items-center mx-4">
+          <div className="w-[100%] px-3 py-2 border border-gray-400 rounded-l-full">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="outline-none"
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+              value={searchQuery}
+            />
+          </div>
+          <button
+            className="px-4 py-2 border border-gray-400 rounded-r-full bg-gray-100"
+            onClick={searchQueryHandler}
+          >
+            <CiSearch size={"24px"} />
+          </button>
+        </div>
+        <IoMdMic
+          size={"42px"}
+          className="ml-3 border border-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-200 duration-200"
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="flex justify-between fixed top-0 w-[100%] bg-white px-6 py-2">
       <div className="flex items-center space-x-4">
-        <AiOutlineMenu className="text-xl cursor-pointer" />
+        <AiOutlineMenu
+          className="text-xl cursor-pointer"
+          onClick={handleSidebar}
+        />
         <img src={logo} alt="yt-logo" className="w-20 cursor-pointer" />
       </div>
-      <div className="flex w-[35%] items-center">
+      <div className="hidden md:flex w-[35%] items-center">
         <div className="w-[100%] px-3 py-2 border border-gray-400 rounded-l-full">
           <input
             type="text"
@@ -51,6 +101,10 @@ function Navbar() {
         />
       </div>
       <div className="flex space-x-5 items-center cursor-pointer">
+        <IoSearch
+          className="text-2xl md:hidden"
+          onClick={() => setSearchBar(!searchBar)}
+        />
         <RiVideoAddLine className="text-2xl" />
         <AiOutlineBell className="text-2xl cursor-pointer" />
         <Avatar src={profile} size="32" round={true} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,212 +21,207 @@ import { PiLightbulbLight } from "react-icons/pi";
 import { SiStylelint } from "react-icons/si";
 import { MdPodcasts } from "react-icons/md";
 import { BiVideo } from "react-icons/bi";
+import { useUtils } from "../context/UtilsContext";
 
 function Sidebar() {
+  const { isSidebar, mobileShow, setMobileShow } = useUtils();
+
   const sidebarItems = [
     {
-      id: 1,
-      name: "Home",
-      icon: <GoHome />,
+      groupName: "Home",
+      groupItems: [
+        {
+          id: 1,
+          name: "Home",
+          icon: <GoHome />,
+        },
+        {
+          id: 2,
+          name: "Shorts",
+          icon: <SiYoutubeshorts />,
+        },
+        {
+          id: 3,
+          name: "Subscriptions",
+          icon: <MdOutlineSubscriptions />,
+        },
+      ],
     },
     {
-      id: 2,
-      name: "Shorts",
-      icon: <SiYoutubeshorts />,
+      groupName: "You",
+      groupItems: [
+        {
+          id: 1,
+          name: "Your Channel",
+          icon: <PiUserSquareThin />,
+        },
+        {
+          id: 2,
+          name: "History",
+          icon: <MdHistory />,
+        },
+        {
+          id: 3,
+          name: "Playlists",
+          icon: <MdOutlineSubscriptions />,
+        },
+        {
+          id: 4,
+          name: "Your Videos",
+          icon: <BiVideo />,
+        },
+        {
+          id: 5,
+          name: "Watch later",
+          icon: <MdOutlineWatchLater />,
+        },
+        {
+          id: 6,
+          name: "Liked videos",
+          icon: <AiOutlineLike />,
+        },
+      ],
     },
     {
-      id: 3,
-      name: "Subscriptions",
-      icon: <MdOutlineSubscriptions />,
+      groupName: "Explore",
+      groupItems: [
+        {
+          id: 1,
+          name: "Trending",
+          icon: <SiTrendmicro />,
+        },
+        {
+          id: 2,
+          name: "Shopping",
+          icon: <HiOutlineShoppingBag />,
+        },
+        {
+          id: 3,
+          name: "Music",
+          icon: <SiYoutubemusic />,
+        },
+        {
+          id: 4,
+          name: "Films",
+          icon: <PiFilmSlateLight />,
+        },
+        {
+          id: 5,
+          name: "Live",
+          icon: <CgMediaLive />,
+        },
+        {
+          id: 6,
+          name: "Gaming",
+          icon: <IoGameControllerOutline />,
+        },
+        {
+          id: 7,
+          name: "News",
+          icon: <FaRegNewspaper />,
+        },
+        {
+          id: 8,
+          name: "Sport",
+          icon: <TfiCup />,
+        },
+        {
+          id: 9,
+          name: "Courses",
+          icon: <SiStylelint />,
+        },
+        {
+          id: 10,
+          name: "Fashion & beauty",
+          icon: <PiLightbulbLight />,
+        },
+        {
+          id: 11,
+          name: "Podcasts",
+          icon: <MdPodcasts />,
+        },
+      ],
+    },
+    {
+      groupName: "More From You Tube",
+      groupItems: [
+        {
+          id: 1,
+          name: "Youtube Premium",
+          icon: <FaYoutube />,
+        },
+        {
+          id: 2,
+          name: "Youtube Studio",
+          icon: <SiYoutubestudio />,
+        },
+        {
+          id: 3,
+          name: "Youtube Music",
+          icon: <SiYoutubemusic />,
+        },
+        {
+          id: 4,
+          name: "Youtube Kids",
+          icon: <SiYoutubekids />,
+        },
+      ],
     },
   ];
-  const sidebarItems2 = [
-    {
-      id: 1,
-      name: "Your Channel",
-      icon: <PiUserSquareThin />,
-    },
-    {
-      id: 2,
-      name: "History",
-      icon: <MdHistory />,
-    },
-    {
-      id: 3,
-      name: "Playlists",
-      icon: <MdOutlineSubscriptions />,
-    },
-    {
-      id: 4,
-      name: "Your Videos",
-      icon: <BiVideo />,
-    },
-    {
-      id: 5,
-      name: "Watch later",
-      icon: <MdOutlineWatchLater />,
-    },
-    {
-      id: 6,
-      name: "Liked videos",
-      icon: <AiOutlineLike />,
-    },
-  ];
-  const sidebarItems3 = [
-    {
-      id: 1,
-      name: "Trending",
-      icon: <SiTrendmicro />,
-    },
-    {
-      id: 2,
-      name: "Shopping",
-      icon: <HiOutlineShoppingBag />,
-    },
-    {
-      id: 3,
-      name: "Music",
-      icon: <SiYoutubemusic />,
-    },
-    {
-      id: 4,
-      name: "Films",
-      icon: <PiFilmSlateLight />,
-    },
-    {
-      id: 5,
-      name: "Live",
-      icon: <CgMediaLive />,
-    },
-    {
-      id: 6,
-      name: "Gaming",
-      icon: <IoGameControllerOutline />,
-    },
-    {
-      id: 7,
-      name: "News",
-      icon: <FaRegNewspaper />,
-    },
-    {
-      id: 8,
-      name: "Sport",
-      icon: <TfiCup />,
-    },
-    {
-      id: 9,
-      name: "Courses",
-      icon: <SiStylelint />,
-    },
-    {
-      id: 10,
-      name: "Fashion & beauty",
-      icon: <PiLightbulbLight />,
-    },
-    {
-      id: 11,
-      name: "Podcasts",
-      icon: <MdPodcasts />,
-    },
-  ];
-  const sidebarItems4 = [
-    {
-      id: 1,
-      name: "Youtube Premium",
-      icon: <FaYoutube />,
-    },
-    {
-      id: 2,
-      name: "Youtube Studio",
-      icon: <SiYoutubestudio />,
-    },
-    {
-      id: 3,
-      name: "Youtube Music",
-      icon: <SiYoutubemusic />,
-    },
-    {
-      id: 4,
-      name: "Youtube Kids",
-      icon: <SiYoutubekids />,
-    },
-  ];
-  return (
-    <div className="px-6 w-[31%] h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
-      {/* Home  */}
-      <div className="space-y-3 items-center">
-        {sidebarItems.map((item) => {
-          return (
-            <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-              <div className="text-xl cursor-pointer">{item.icon}</div>
-              <span className="cursor-pointer">{item.name}</span>
-            </div>
-          );
-        })}
-      </div>
-      <br />
-      <hr />
-      {/* You Part  */}
-      <div className="mt-4 space-y-3 items-center">
-        <div className="flex items-center spacx-2">
-          <h1>You</h1>
-          <FaChevronRight />
-        </div>
-        {sidebarItems2.map((item) => {
-          return (
-            <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-              <div className="text-xl cursor-pointer">{item.icon}</div>
-              <span className="cursor-pointer">{item.name}</span>
-            </div>
-          );
-        })}
-      </div>
-      <br />
-      <hr />
-      {/* for explore */}
-      <div className="mt-4 space-y-3 items-center">
-        <div className="items-center spacx-2">
-          <h1 className="font-semibold">Explore</h1>
-        </div>
-        {sidebarItems3.map((item) => {
-          return (
-            <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-              <div className="text-xl cursor-pointer">{item.icon}</div>
-              <span className="cursor-pointer">{item.name}</span>
-            </div>
-          );
-        })}
-      </div>
-      <br />
-      <hr />
 
-      {/* More Section  */}
-      <div className="mt-4 space-y-3 items-center">
-        <div className="items-center spacx-2">
-          <h1 className="font-semibold">More From Youtube</h1>
-        </div>
-        {sidebarItems4.map((item) => {
-          return (
-            <div key={item.id} className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1">
-              <div className="text-xl cursor-pointer text-red-800">
-                {item.icon}
-              </div>
-              <span className="cursor-pointer">{item.name}</span>
-            </div>
-          );
-        })}
-        <hr />
+  const ModelOverlay = ()=>{
+    return(
+      <div className="flex fixed top-0 right-0 bottom-0 left-0 bg-black/50 z-30"
+      onClick={()=>setMobileShow(!mobileShow)}>
       </div>
-      <br />
-      <span className="text-xs font-semibold text-gray-600">
-        About Press Copyright<br />Contact us Creator<br />Advertise
-        Developers <br /><br />
-        <p>TermsPrivacyPolicy & Safety <br /> How YouTube works <br />Test new features</p>
-      </span>
-      <br />
-      <p className="text-xs text-gray-800 mt-1">© 2024 Google LLC
-      </p>
-    </div>
+    );
+  }
+  return (
+    <>
+      <div className={`${
+        mobileShow?"fixed top-0 bottom-0 left-0 transition-all duration-300 bg-white z-40 h-screen w-[70%] sm:w-[30%]"
+        :"hidden h-[calc(100vh-6.625rem)] w-[18%]"
+        } xl:static xl:block px-2 lg:px-6 overflow-y-scroll overflow-x-hidden scrollbar-thin`}>
+        {/* Home  */}
+        <div className="space-y-3 items-center">
+          {sidebarItems.map((group) => {
+            return (
+              <div className="mb-5">
+                <h1 className="font-bold">{group.groupName}</h1>
+                {group.groupItems.map((item) => (
+                  <div
+                    key={item.id}
+                    className="flex items-center space-x-6 hover:bg-gray-300 duration-300 rounded-xl p-1 my-3"
+                  >
+                    <div className="text-xl cursor-pointer">{item.icon}</div>
+                    <span className="text sm cursor-pointer">{item.name}</span>
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+        </div>
+        <br />
+        <hr />
+        <br />
+        <span className="text-xs font-semibold text-gray-600">
+          About Press Copyright
+          <br />
+          Contact us Creator
+          <br />
+          Advertise Developers <br />
+          <br />
+          <p>
+            TermsPrivacyPolicy & Safety <br /> How YouTube works <br />
+            Test new features
+          </p>
+        </span>
+        <br />
+        <p className="text-xs text-gray-800 mt-1">© 2024 Google LLC</p>
+      </div>
+
+      {mobileShow ?<ModelOverlay/> : null}
+    </>
   );
 }
 
